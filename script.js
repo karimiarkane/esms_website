f30d2944e5ecc53db1ce14ee4ac5abee583b3cd7
karimiarkane <karimiarkane6@gmail.com>
2022-12-08 18:12:40 +0000

    time section

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ buttons.forEach(function (elm, index, array) {
             elem.classList.add("inactive");
             elem.setAttribute("id","");
         })
-      
+     
         questions[index].setAttribute("id","active");
-
+        let active_q = questions[index];
     }
 });
-let active_sign =  document.querySelector("#active");
-console.log(active_sign)
+
+console.log(active_q)
 /*active_sign.setAttribute("src","./images/substract(1).svg");*/
 
 
