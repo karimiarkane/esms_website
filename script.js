da1082ddcccb8cedf80d7870ee5f8372bcf3a641
rayane <mr_idja@esi.dz>
2022-12-09 15:20:33 +0100

    fixed scedule

--- script.js
+++ script.js
@@ -47,22 +47,23 @@ var next1 = document.getElementById("next1");
 var arr1 = [
   {
     title: "Day1",
-    d1: "8:00-10:00 presentation of the event",
-    d2: "10:00-12:00 presentation of the sponsors",
-    d3: "13:00 coffee break and lauch of the show",
+    d1: "9:00-10:00 Reception",
+    d2: "10:00-10:30 ceremonie d'ouverture",
+    d3: "10:30-12:00 conferences",
+    d4: "12:00-13:30 Pause dejeuner",
+    d5: "13:30-15:00 Deux ateliers en parallele",
+    d6: "15:00-16:00 Pause cafe",
+    d7: "16:00-17:00 Atelier",
   },
   {
     title: "Day2",
-    d1: "8:00-11:00 competition continues",
-    d2: "11:15-12:30 lunch break",
-    d3: "13:00 resume the competition",
-  },
-  ,
-  {
-    title: "Day3",
-    d1: "8:00-11:00 continuing the event ",
-    d2: "12:00 winners announcement",
-    d3: "13:00 closing ceremony",
+    d1: "8:30-9:00 Reception",
+    d2: "9:00-9:30 ceremonie d'ouverture 2",
+    d3: "9:30-12:00 competition",
+    d4: "12:00-13:00 Pause dejeuner",
+    d5: "13:00-15:00 competition",
+    d6: "15:00-16:00 Pause cafe +jugement",
+    d7: "16:00-17:00 Ceremonie de cloture",
   },
 ];
 var i = 0;
@@ -70,26 +71,38 @@ document.getElementById("Day").innerHTML = arr1[0].title;
 document.getElementById("task1").innerHTML = arr1[0].d1;
 document.getElementById("task2").innerHTML = arr1[0].d2;
 document.getElementById("task3").innerHTML = arr1[0].d3;
+document.getElementById("task4").innerHTML = arr1[0].d4;
+document.getElementById("task5").innerHTML = arr1[0].d5;
+document.getElementById("task6").innerHTML = arr1[0].d6;
+document.getElementById("task7").innerHTML = arr1[0].d7;
 
 next1.addEventListener("click", function () {
   i++;
-  if (i > arr.length - 1) {
+  if (i > arr1.length - 1) {
     i = 0;
   }
   document.getElementById("Day").innerHTML = arr1[i].title;
   document.getElementById("task1").innerHTML = arr1[i].d1;
   document.getElementById("task2").innerHTML = arr1[i].d2;
   document.getElementById("task3").innerHTML = arr1[i].d3;
+  document.getElementById("task4").innerHTML = arr1[i].d4;
+  document.getElementById("task5").innerHTML = arr1[i].d5;
+  document.getElementById("task6").innerHTML = arr1[i].d6;
+  document.getElementById("task7").innerHTML = arr1[i].d7;
 });
 prev1.addEventListener("click", function () {
   i--;
   if (i < 0) {
-    i = arr.length - 1;
+    i = arr1.length - 1;
   }
   document.getElementById("Day").innerHTML = arr1[i].title;
   document.getElementById("task1").innerHTML = arr1[i].d1;
   document.getElementById("task2").innerHTML = arr1[i].d2;
   document.getElementById("task3").innerHTML = arr1[i].d3;
+  document.getElementById("task4").innerHTML = arr1[i].d4;
+  document.getElementById("task5").innerHTML = arr1[i].d5;
+  document.getElementById("task6").innerHTML = arr1[i].d6;
+  document.getElementById("task7").innerHTML = arr1[i].d7;
 });
 
 var prev = document.getElementById("previous");
