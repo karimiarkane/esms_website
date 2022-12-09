d24942075026b0ee3f93621a64eb726677129d0e
bensalem14 <104575447+bensalem14@users.noreply.github.com>
2022-12-09 09:13:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,82 @@ var x = setInterval(function () {
     document.getElementById("day").innerHTML = "expired";
   }
 }, 1000);
+var prev1 = document.getElementById("previous1");
+var next1 = document.getElementById("next1");
+var arr1 = [
+  {
+    title: "Day1",
+    d1: "8:00-10:00 presentation of the event",
+    d2: "10:00-12:00 presentation of the sponsors",
+    d3: "13:00 coffee break and lauch of the show",
+  },
+  {
+    title: "Day2",
+    d1: "8:00-11:00 competition continues",
+    d2: "11:15-12:30 lunch break",
+    d3: "13:00 resume the competition",
+  },
+  ,
+  {
+    title: "Day3",
+    d1: "8:00-11:00 continuing the event ",
+    d2: "12:00 winners announcement",
+    d3: "13:00 closing ceremony",
+  },
+];
+var i = 0;
+document.getElementById("Day").innerHTML = arr1[0].title;
+document.getElementById("task1").innerHTML = arr1[0].d1;
+document.getElementById("task2").innerHTML = arr1[0].d2;
+document.getElementById("task3").innerHTML = arr1[0].d3;
+
+next1.addEventListener("click", function () {
+  i++;
+  if (i > arr.length - 1) {
+    i = 0;
+  }
+  document.getElementById("Day").innerHTML = arr1[i].title;
+  document.getElementById("task1").innerHTML = arr1[i].d1;
+  document.getElementById("task2").innerHTML = arr1[i].d2;
+  document.getElementById("task3").innerHTML = arr1[i].d3;
+});
+prev1.addEventListener("click", function () {
+  i--;
+  if (i < 0) {
+    i = arr.length - 1;
+  }
+  document.getElementById("Day").innerHTML = arr1[i].title;
+  document.getElementById("task1").innerHTML = arr1[i].d1;
+  document.getElementById("task2").innerHTML = arr1[i].d2;
+  document.getElementById("task3").innerHTML = arr1[i].d3;
+});
+
+var prev = document.getElementById("previous");
+var next = document.getElementById("next");
+
+var arr = [
+  "images/oredoo.png",
+  "images/sonatrach.png",
+  "images/intel.png",
+  "images /nvidia.png",
+];
+
+i = 0;
+
+document.getElementById("frame").src = arr[0];
+
+next.addEventListener("click", function () {
+  i++;
+  if (i > arr.length - 1) {
+    i = 0;
+  }
+  document.getElementById("frame").src = arr[i];
+});
+
+prev.addEventListener("click", function () {
+  i--;
+  if (i < 0) {
+    i = arr.length - 1;
+  }
+  document.getElementById("frame").src = arr[i];
+});
